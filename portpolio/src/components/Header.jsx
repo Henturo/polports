c4a0840b43fe9f08736882bf07cbfee79051909b
Henturo <pauljohnpalamara123@gmail.com>
2025-11-24 00:02:47 +0800

    Added File

--- portpolio/src/components/Header.jsx
+++ portpolio/src/components/Header.jsx
@@ -5,13 +5,14 @@ function Header() {
     <header className="header">
       <div className="profile-photo-wrapper">
         {/* Provide your photo in public/profile.jpg */}
-        <img src="/charly.jpg" alt="Profile" className="profile-photo" />
+        <img src="/profile.jpg" alt="Profile" className="profile-photo" />
       </div>
       <div>
         <h1>Paul John Palamara</h1>
         <nav>
           <a href="#about">About</a>
           <a href="#skills">Skills</a>
+          <a href="#projects">Projects</a>
           <a href="#contact">Contact</a>
         </nav>
       </div>
